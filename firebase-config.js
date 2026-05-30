30882d44bd366c3012f8725204352a7d0bbdaa17
Minkyung Shin <minkyungshin@Minkyungui-MacBookPro.local>
2026-05-30 17:58:36 +0900

    feat: add menu ordering, cart, order list page with Firebase

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase 프로젝트 설정
+// Firebase Console(https://console.firebase.google.com)에서 프로젝트 생성 후
+// 프로젝트 설정 > 앱 추가 > 웹 에서 아래 값을 교체하세요.
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
